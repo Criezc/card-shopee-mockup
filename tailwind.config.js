5cc11fd8495bebb87f10500bdd671506fb866246
friscolie <friscolieyanto@gmail.com>
2021-10-11 23:18:00 +0700

    commit v1

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#ee4d2d",
+        secondaryColor: "#f69113",
+        thirdColor: "#d0011b",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
